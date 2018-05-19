9c357958361f176fac5fc086269bc3f26902f2b7
GIORGIA <m.tandoi4@studenti.uniba.it>
2018-05-19 21:40:54 +0200

    Almost finished

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import csv
 import pprint
 
 """-----------------------------------------------------CONSTANTS DECLARATION-------------------------- """
+ID_Field = '_id'
 movieId = 'movieId'
 title = 'title'
 genres = 'genres'
 
 userId = 'userId'
-rating = 'rating'
+rating_value = 'rating'
 timestamp = 'timestamp'
 
 imdbId = 'imdbId'
@@ -19,59 +20,30 @@ tmdbId = 'tmdbId'
 
 tag = 'tag'
 
-movies_fields = ['_id', title, genres]
-ratings_fields = [userId, movieId, rating, timestamp]
+movies_fields = [ID_Field, title, genres]
+movie_field_avg = 'averageRating'
+movie_field_count = 'numberOfRatings'
+ratings_fields = [userId, movieId, rating_value, timestamp]
 links_fields = [movieId, imdbId, tmdbId]
 tags_fields = [userId, movieId, tag, timestamp]
-"""
-def parse_int(string):
-    return int(''.join([x for x in string if x.isdigit()])) """
 
-"""This function updates a collection adding new entries or creates it if it doesn't exists.
- It takes three parameters: collection which is the collection object to which we want to add entries; 
- input_csv which is a csv file containing the entries to add, this file is converted to a dictionary python type 
- which can be stored in mongodb as a sequence of documents belonging to the same collection; last but not least, 
- the fields parameter is a sequence of parameters that represent the JSON fields of the specific collection. 
+RATINGS_SOURCE_FILE = 'data/ratings.csv'
+MOVIES_SOURCE_FILE = 'data/movies.csv'
+TAGS_SOURCE_FILE = 'data/tags.csv'
+LIKS_SOURCE_FILE = 'data/links.csv'
 
-
-
-def import_collection(collection, input_csv, fields):
-
-    with open(input_csv, newline='\n', encoding='utf8') as csvfile:
-        data = csv.reader(csvfile)
-        output = []
-
-        for row in data:
-            i = 0
-            result = {}
-            for field in fields:
-                result[field] = row[i]
-                i += 1
-            output.append(result)
-
-    try:
-        just_inserted = collection.insert_many(output)
-
-    except BulkWriteError as bwe:
-        print(bwe.details)
-        raise
-
-    return just_inserted
-
- """
 """----------------------------------------- CONNECTING TO MONGODB---------------------------------------------
 MongoClient() retrieves the mongodb instance of the running server, which, in this case, is the default one, 
 running on the localhost on port 27017
-
 client.moviesDB retrieves a reference to a mongodb database instance (moviesDB). If the database doesn't exists it
 will be created"""
 
 client = MongoClient()
-db = client.moviesDB
+MOVIES_DB = client.moviesDB
 
-collection = db.ratings
-collection.drop()
-csvfile = open('data/ratings.csv', 'r', newline='\n', encoding='utf8')
+
+MOVIES_DB.ratings.drop()
+csvfile = open(RATINGS_SOURCE_FILE, 'r', newline='\n', encoding='utf8')
 data = csv.reader(csvfile)
 outcome = []
 
@@ -94,13 +66,13 @@ for row in data:
         i += 1
     outcome.append(current_document_to_insert)
 
-inserted = collection.insert_many(outcome)
+
+MOVIES_DB.ratings.insert_many(outcome)
 
 
-collection = db.movies
-collection.drop()
+MOVIES_DB.movies.drop()
 
-csvfile = open('data/movies.csv', 'r', newline='\n', encoding='utf8')
+csvfile = open(MOVIES_SOURCE_FILE, 'r', newline='\n', encoding='utf8')
 data = csv.reader(csvfile)
 outcome = []
 
@@ -111,8 +83,8 @@ with the $group stage or db.collection.mapReduce() instead.
 """
 
 
-pipeline = [{"$group": {"_id": "$movieId", "averageRate": {"$avg": "$rating"}, "count": {"$sum": 1}}}]
-cursor = db.ratings.aggregate(pipeline, cursor={})
+pipeline = [{"$group": {"_id": "$movieId", "averageRating": {"$avg": "$rating"}, "count": {"$sum": 1}}}]
+cursor = MOVIES_DB.ratings.aggregate(pipeline, cursor={})
 
 group_result_list = []
 for doc in cursor:
@@ -121,7 +93,7 @@ for doc in cursor:
 
 def find(list_of_documents, identifier):
     for d in list_of_documents:
-        if d['_id'] == identifier:
+        if d[ID_Field] == identifier:
             return d
     return 0
 
@@ -136,16 +108,41 @@ for row in data:
         except ValueError:
             current_document_to_insert[field] = row[i]
         i += 1
-    calculated_fields = find(group_result_list, current_document_to_insert['_id'])
+    calculated_fields = find(group_result_list, current_document_to_insert[ID_Field])
     if calculated_fields != 0:
-        current_document_to_insert['averageRate'] = calculated_fields['averageRate']
-        current_document_to_insert['count'] = calculated_fields['count']
+        current_document_to_insert[movie_field_avg] = round(calculated_fields['averageRating'], 2)
+        current_document_to_insert[movie_field_count] = calculated_fields['count']
 
     outcome.append(current_document_to_insert)
 
 try:
-    db.movies.insert_many(outcome)
+    MOVIES_DB.movies.insert_many(outcome)
 except BaseException as bwe:
         print(bwe.details)
 
+
+"""-------------------------LOADING TAGS COLLECTION-------------------------------------------"""
+
+MOVIES_DB.tags.drop()
+
+csvfile = open(TAGS_SOURCE_FILE, 'r', newline='\n', encoding='utf8')
+data = csv.reader(csvfile)
+outcome = []
+next(data)
+for row in data:
+    i = 0
+    current_document_to_insert = {}
+    for field in tags_fields:
+            try:
+                current_document_to_insert[field] = int(row[i])
+            except ValueError:
+                current_document_to_insert[field] = row[i]
+            i += 1
+    outcome.append(current_document_to_insert)
+
+try:
+    MOVIES_DB.tags.insert_many(outcome)
+except BulkWriteError as bwe:
+        print(bwe.details)
+        raise
 
---
+++ other_version.py
@@ -0,0 +1,248 @@
+__author__ = 'Giorgia'
+
+from pymongo import MongoClient
+from pymongo.errors import BulkWriteError, ConnectionFailure
+import csv
+from bson.dbref import DBRef
+
+"""-----------------------------------------------------CONSTANTS DECLARATION-------------------------- """
+ID_Field='_id'
+movieId = 'movieId'
+title = 'title'
+genres = 'genres'
+
+userId = 'userId'
+rating_value = 'rating'
+timestamp = 'timestamp'
+
+imdbId = 'imdbId'
+tmdbId = 'tmdbId'
+
+tag = 'tag'
+
+movies_fields = [ID_Field, title, genres]
+movie_field_avg = 'averageRating'
+movie_field_count = 'numberOfRatings'
+ratings_fields = [userId, movieId, rating_value, timestamp]
+links_fields = [movieId, imdbId, tmdbId]
+tags_fields = [userId, movieId, tag, timestamp]
+
+RATINGS_SOURCE_FILE = 'data/ratings.csv'
+MOVIES_SOURCE_FILE = 'data/movies.csv'
+TAGS_SOURCE_FILE = 'data/tags.csv'
+LINKS_SOURCE_FILE = 'data/links.csv'
+
+"""----------------------------------------- CONNECTING TO MONGODB---------------------------------------------
+MongoClient() retrieves the mongodb instance of the running server, which, in this case, is the default one, 
+running on the localhost on port 27017
+
+client.moviesDB retrieves a reference to a mongodb database instance (moviesDB). If the database doesn't exists it
+will be created"""
+
+try:
+    client = MongoClient()
+    MOVIES_DB = client.moviesDB
+except ConnectionFailure:
+    print('something went wrong when trying to connect to mongodb')
+    exit(0)
+
+
+"""
+-------------------------------------------LOADING RATINGS COLLECTION-------------------------------------------
+"""
+
+MOVIES_DB.ratings.drop()  # this command drops the collection if exists, so that we can run this script any time.
+# Without this command mongodb would reject the insertion and the script would crash,
+# since it doesn't accept documents with an already existing ID
+
+
+with open(RATINGS_SOURCE_FILE, newline='\n', encoding='utf8') as csv_file:
+    data = csv.reader(csv_file)
+    outcome = []
+    next(data)  # with this command removes the header of the csv file
+    """Now we can loop over the just read file. Since all the values in each line of the file are 
+     read as string values, some cast operations are needed"""
+    for row in data:
+        i = 0
+        current_document_to_insert = {}
+        for field in ratings_fields:
+            if field == rating_value:
+                try:
+                    current_document_to_insert[field] = float(row[i])  # this cast operation is necessary
+                    # since we need to compute calculations with rating values
+                except ValueError:
+                    current_document_to_insert[field] = row[i]
+            else:
+                try:
+                    current_document_to_insert[field] = int(row[i])  # this line casts the input fields to a numeric
+                    # type if an int value is recognised. Substituting string values with numeric types makes operating
+                    # on the DB more efficient. For example indexing the ID field
+                except ValueError:
+                    current_document_to_insert[field] = row[i]  # if the value is not a numeric one, than it is added as
+                    # it is
+            i += 1
+
+        outcome.append(current_document_to_insert)
+
+
+"""Now that we filled to outcome variable with a list of json-like objects, we can add the list to the ratings 
+collection"""
+try:
+    MOVIES_DB.ratings.insert_many(outcome)
+except BulkWriteError as bwe:
+        print(bwe.details)
+        raise
+
+"""-------------------------------------------LOADING MOVIES COLLECTION----------------------------------------------"""
+
+MOVIES_DB.movies.drop()
+
+"""
+The two lines below, execute a grouping query to the ratings collection, so that the calculated 
+values can then be added as fields of the movies collection. 
+Mongodb 3.4 deprecates the db.collection.group() method. The method db.collection.aggregate() 
+with the $group stage or db.collection.mapReduce() should be used instead.
+"""
+
+pipeline = [{"$group": {"_id": "$movieId", "averageRating": {"$avg": "$rating"}, "count": {"$sum": 1}}}]
+cursor = MOVIES_DB.ratings.aggregate(pipeline, cursor={})
+
+"""The method 'aggregate' retrieves a cursor object. We can loop over cursors only once, but since we need
+to loop over the resulted list for every document we add to the movies collection, this for loop
+creates a list object with the items in the cursor, so that we can loop over the items every time we want to."""
+group_result_list = []
+for doc in cursor:
+    group_result_list.append(doc)
+
+
+"""This for loop has been wrapped in a function so that we don't have to add an other for in the already two 
+ nested for loops int the lines below. """
+
+
+def find(list_of_documents, identifier):
+    for d in list_of_documents:
+        if d[ID_Field] == identifier:
+            return d
+    return 0
+
+
+with open(MOVIES_SOURCE_FILE, newline='\n', encoding='utf8') as csv_file:
+    data = csv.reader(csv_file)
+    outcome = []
+    next(data)
+    for row in data:
+        i = 0
+        current_document_to_insert = {}
+        for field in movies_fields:
+            if field == genres:
+                current_document_to_insert[field] = row[i].split('|')
+            else:
+                try:
+                    current_document_to_insert[field] = int(row[i])
+                except ValueError:
+                    current_document_to_insert[field] = row[i]
+            i += 1
+        calculated_fields = find(group_result_list, current_document_to_insert[ID_Field])
+        if calculated_fields != 0:
+            current_document_to_insert[movie_field_avg] = round(calculated_fields['averageRating'], 2)
+            current_document_to_insert[movie_field_count] = calculated_fields['count']
+
+        outcome.append(current_document_to_insert)
+
+try:
+    MOVIES_DB.movies.insert_many(outcome)
+except BulkWriteError as bwe:
+        print(bwe.details)
+        raise
+
+
+"""-------------------------------------- LOADING TAGS COLLECTION----------------------------------------------------"""
+
+MOVIES_DB.tags.drop()
+
+with open(TAGS_SOURCE_FILE, newline='\n', encoding='utf8') as csv_file:
+    data = csv.reader(csv_file)
+    outcome = []
+    next(data)
+    for row in data:
+        i = 0
+        current_document_to_insert = {}
+        for field in tags_fields:
+                try:
+                    current_document_to_insert[field] = int(row[i])
+                except ValueError:
+                    current_document_to_insert[field] = row[i]
+                i += 1
+
+        outcome.append(current_document_to_insert)
+
+
+try:
+    MOVIES_DB.tags.insert_many(outcome)
+except BulkWriteError as bwe:
+        print(bwe.details)
+        raise
+
+
+"""-----------------------------------------------UPDATING MOVIES WITH NEW TAGS---------------------------------"""
+
+
+def remove_movie_id_field(tag_document_list):
+    for tag_document in tag_document_list:
+        del tag_document[movieId]
+
+
+movie_field_tags = 'tags'
+print(MOVIES_DB.movies.find())
+for movie in list(MOVIES_DB.movies.find()):
+    tags = list(MOVIES_DB.tags.find({movieId: movie[ID_Field]}))
+    if len(tags) != 0:
+        remove_movie_id_field(tags)
+        MOVIES_DB.movies.update({ID_Field: movie[ID_Field]}, {"$set": {movie_field_tags: tags}}, upsert=False)
+
+
+""" ----------------------------------------------LOADING LINKS IN AN OTHER DATABASE-------------------------------"""
+movies_collection = "movies"
+movies_database = "moviesDB"
+WIDER_MOVIES_DB = client.widerMoviesDb
+
+
+WIDER_MOVIES_DB.links.drop()
+
+with open(LINKS_SOURCE_FILE, newline='\n', encoding='utf8') as csv_file:
+    data = csv.reader(csv_file)
+    outcome = []
+    next(data)
+    for row in data:
+        i = 0
+        current_document_to_insert = {}
+        for field in links_fields:
+            if field == movieId:
+                current_document_to_insert[field] = DBRef(movies_collection, row[i], movies_database)
+            else:
+                try:
+                    current_document_to_insert[field] = int(row[i])
+                except ValueError:
+                    current_document_to_insert[field] = row[i]
+            i += 1
+
+        outcome.append(current_document_to_insert)
+
+
+try:
+    WIDER_MOVIES_DB.links.insert_many(outcome)
+except BulkWriteError as bwe:
+        print(bwe.details)
+        raise
+
+"""-------------------------------------------DATA ACCESS ON MOVIES COLLECTION-----------------------------"""
+
+# CREATING THE INDEX
+
+MOVIES_DB.movies.create_index(movie_field_tags)
+tag_inner_field = 'tag'
+
+first_query = list(MOVIES_DB.movies.find({movie_field_tags: {'$elemMatch': {tag_inner_field: "love"}}}))
+
+for row_result in first_query:
+    print(row_result)
